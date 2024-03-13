f05c5085aabbb85c93f16f826ef0f4786efd8f9d
tursky <tursky.sv@protonmail.com>
2024-03-13 13:36:08 +0300

    Refactor, implement error handler

--- src/server.js
+++ src/server.js
@@ -1,20 +1,22 @@
 // Fetch microservice
 
-async function handler(request) {
+const handler = async (request) => {
   let GET = request['url'];
 
   const isClientURI = GET.includes('=');
   if (isClientURI === false) GET = '=nasa.gov';
   const domain = GET.split('=')[1]; // microservice.dev/?req=domain
   const url = `http://${domain}`;
-  const fetchResponse = await fetch(url);
-  const html = await fetchResponse.text();
 
-  return new Response(html, {
-    headers: {
-      'content-type': 'text/html',
-    },
-  });
-}
+  try {
+    const fetchResponse = await fetch(url);
+    const html = await fetchResponse.text();
+    return new Response(html, { headers: { 'content-type': 'text/html' } });
+  } catch (err) {
+    if (err instanceof TypeError) {
+      return new Response("Don't worry");
+    }
+  }
+};
 
-Deno.serve(handler, { port: 8080 });
+Deno.serve(handler, { port: 8000 });
